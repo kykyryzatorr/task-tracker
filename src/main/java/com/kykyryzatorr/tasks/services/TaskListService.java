21bfa519ddc4f0888c85225b1390776be3ead725
kykyryzatorr <rsnazarovv@yandex.ru>
2026-04-04 05:01:11 +0300

    Add other endpoints into a single controller

--- src/main/java/com/kykyryzatorr/tasks/services/TaskListService.java
+++ src/main/java/com/kykyryzatorr/tasks/services/TaskListService.java
@@ -3,9 +3,19 @@ package com.kykyryzatorr.tasks.services;
 import com.kykyryzatorr.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
 
     List<TaskList> listTaskLists();
+
+    TaskList createTaskList(TaskList taskList);
+
+    Optional<TaskList> getTaskList(UUID id);
+
+    TaskList updateTaskList(UUID taskListId, TaskList taskList);
+
+    void deleteTaskList(UUID taskListId);
 
 }
--- src/main/java/com/kykyryzatorr/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/kykyryzatorr/tasks/services/impl/TaskListServiceImpl.java
@@ -3,9 +3,14 @@ package com.kykyryzatorr.tasks.services.impl;
 import com.kykyryzatorr.tasks.domain.entities.TaskList;
 import com.kykyryzatorr.tasks.repositories.TaskListRepository;
 import com.kykyryzatorr.tasks.services.TaskListService;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TaskListServiceImpl implements TaskListService {
@@ -19,5 +24,54 @@ public class TaskListServiceImpl implements TaskListService {
     @Override
     public List<TaskList> listTaskLists() {
         return taskListRepository.findAll();
+    }
+
+    @Override
+    public TaskList createTaskList(TaskList taskList) {
+        if (taskList.getId() != null) {
+            throw new IllegalArgumentException("У списка задач уже был идентификатор");
+        }
+        if (taskList.getTitle() == null || taskList.getTitle().isBlank()) {
+            throw new IllegalArgumentException("У списка задач должен быть заголовок");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        return taskListRepository.save(new TaskList(
+                null,
+                taskList.getTitle(),
+                taskList.getDescription(),
+                null,
+                now,
+                now
+        ));
+    }
+
+    @Override
+    public Optional<TaskList> getTaskList(UUID id) {
+        return taskListRepository.findById(id);
+    }
+
+    @Override
+    public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
+        if (taskList.getId() == null) {
+            throw new IllegalArgumentException("Лист задач должен иметь id");
+        }
+
+        if (!Objects.equals(taskListId, taskList.getId())) {
+            throw new IllegalArgumentException("");
+        }
+
+        TaskList existingTaskList = taskListRepository.findById(taskListId).orElseThrow(() ->
+                new IllegalArgumentException("Лист задач не найден")
+        );
+        existingTaskList.setTitle(taskList.getTitle());
+        existingTaskList.setDescription(taskList.getDescription());
+        existingTaskList.setUpdated(LocalDateTime.now());
+        return taskListRepository.save(existingTaskList);
+    }
+
+    @Override
+    public void deleteTaskList(UUID taskListId) {
+        taskListRepository.deleteById(taskListId);
     }
 }
